b2bd5313d936323ac089fbd8c7d90094290ab20f
Deo silaen <deokeldisilaen@gmail.com>
2025-03-15 11:40:34 +0700

    fix gunShoot and add anim water

--- Assets/Scripts/GunShoot.cs
+++ Assets/Scripts/GunShoot.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class GunShoot : MonoBehaviour
 {
-    public GameObject bulletPrefab;
+    public GameObject bulletHolePrefab;
     Camera mainCamera;
+
+    float timeDestroy = 10f;
 
     void Start()
     {
         mainCamera = Camera.main;
+        Cursor.visible = false;
     }
     void Update()
     {
@@ -19,8 +22,26 @@ public class GunShoot : MonoBehaviour
 
     void Shoot()
     {
-        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0;
-        GameObject bullet = Instantiate(bulletPrefab, mousePosition, Quaternion.identity);
+        Debug.Log("Masuk");
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Kena: " + hit.collider.gameObject.name);
+            GameObject bulletHole = Instantiate(bulletHolePrefab, hit.point, Quaternion.identity);
+
+            if (hit.collider.CompareTag("Target"))
+            {
+                Destroy(hit.collider.gameObject, timeDestroy);
+                Destroy(bulletHole, timeDestroy);
+            }
+        }
+        else
+        {
+            Debug.Log("Raycast tidak kena apa-apa!");
+        }
     }
+
 }
